3b76b056055b82f68297ed6505d901d000b8cf36
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 22:21:44 -0300

    Exercício 1 (Variavel de acesso) - Apostila

---
+++ script.js
@@ -0,0 +1,9 @@
+let nivelAcesso = 2;
+
+if (nivelAcesso === 3) {
+  console.log("Acesso Total");
+} else if (nivelAcesso === 2) {
+  console.log("Acesso Parcial");
+} else {
+  console.log("Acesso Negado");
+}
